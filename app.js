f20b80be576a47f36969e7b49e362099507ed5ab
Ashmeet670 <73339721+Ashmeet670@users.noreply.github.com>
2024-09-14 19:12:41 +0530

    playing bar updated

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 bar = document.getElementById("progress-bar")
 bar.style.width = "95%";
+
+function togglePlayPause() {
+    document.getElementById("play").classList.toggle("d-none")
+    document.getElementById("pause").classList.toggle("d-none")
+    
+}
