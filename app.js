52ed8e8adc6a4bc712e12ec154b7b1b2d12bdcc3
Ashmeet670 <73339721+Ashmeet670@users.noreply.github.com>
2024-09-15 01:08:42 +0530

    audio playing attempt
    this has failed...will now try with audio html element way

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
 bar = document.getElementById("progress-bar")
 bar.style.width = "95%";
 
+
+
 function togglePlayPause() {
     document.getElementById("play").classList.toggle("d-none")
     document.getElementById("pause").classList.toggle("d-none")
     
 }
 
+playing = false
+nowPlaying = ''
+toPlay = ''
+var audio
 
-function play(){
-    document.getElementById("creator").play()
-}
+
+// function playSong(url){
+//     toPlay = url
+//     if(playing){
+//         audio.pause()
+//         console.log("paused")
+//     }
+//     else{
+//         console.log("playing")
+//         audio = new Audio(url).play()
+//         playing = true
+//     }
+// }
+
+// function playNow(){
+//     if(playing){
+//         console.log("playingNew")
+//         if(nowPlaying === toPlay){
+//             //do nothing
+//         }
+//         else if(nowPlaying != toPlay){
+
+//             console.log("pauseingn playing")
+
+//             audio.pause()
+//             audio.currentTime = 0
+//             playing = true
+//         }
+//     }
+//     else{
+//         console.log("playingNew1")
+//         audio = new Audio(toPlay).play()
+//         playing = true
+
+//     }
+// }
