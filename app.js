33689865e5590f7b8d19523451031cfc9603b3c3
Ashmeet670 <73339721+Ashmeet670@users.noreply.github.com>
2024-09-15 17:58:16 +0530

    added all music cards and playing

--- app.js
+++ app.js
@@ -5,29 +5,29 @@ nowPlayingDiscName = document.getElementById("nowPlayingDiscName")
 nowPlayingByName = document.getElementById("nowPlayingByName")
 
 discs = {
-    "5": document.getElementById("5"),
-    "11": document.getElementById("11"),
-    "13": document.getElementById("13"),
-    "Blocks": document.getElementById("blocks"),
-    "Cat": document.getElementById("cat"),
-    "Chirp": document.getElementById("chirp"),
+        "Creator": document.getElementById("creator"),
     "Creator (Music Box)": document.getElementById("creator_music_box"),
-    "Creator": document.getElementById("creator"),
-    "Mall": document.getElementById("mall"),
-    "Mellohi": document.getElementById("mellohi"),
+    "Precipice": document.getElementById("precipice"),
+    "Relic": document.getElementById("relic"),
+    "5": document.getElementById("5"),
     "Otherside": document.getElementById("otherside"),
     "Pigstep": document.getElementById("pigstep"),
-    "Precipice": document.getElementById("precipice"),
-    "Record": document.getElementById("record"),
-    "Relic": document.getElementById("relic"),
-    "Stall": document.getElementById("stall"),
+    "Cat": document.getElementById("cat"),
+    "Chirp": document.getElementById("chirp"),
+    "Blocks": document.getElementById("blocks"),
+    "Mall": document.getElementById("mall"),
+    "Mellohi": document.getElementById("mellohi"),
+    "Far": document.getElementById("far"),
+    "Stal": document.getElementById("stal"),
     "Strad": document.getElementById("strad"),
     "Wait": document.getElementById("wait"),
     "Ward": document.getElementById("ward"),
+    "11": document.getElementById("11"),
+    "13": document.getElementById("13"),
 }
 
 discsBy = {
-    "5" : "by Samuel Åberg",
+    "5": "by Samuel Åberg",
     "11": "by C418",
     "13": "by C418",
     "Blocks": "by C418",
@@ -40,28 +40,42 @@ discsBy = {
     "Otherside": "by Lena Raine",
     "Pigstep": "by Lena Raine",
     "Precipice": "by Aaron Cherof",
-    "Record": "by C418",
+    "Far": "by C418",
     "Relic": "by Aaron Cherof",
-    "Stall": "by C418",
+    "Stal": "by C418",
     "Strad": "by C418",
     "Wait": "by C418",
     "Ward": "by C418",
 }
 
+autoPlayOrder = ["Creator", "Creator (Music Box)", "Precipice", "Relic", "5", "Otherside", "Pigstep", "Cat", "Chirp", "Blocks", "Mall", "Mellohi", "Far", "Stal", "Strad", "Wait", "Ward", "11", "13"]
+
 playing = "5"
 isPlaying = false
 
+
+playTextIsPlaying = document.getElementById("5Play")
+
 function barLenght() {
-    bar.style.width = `${((discs[playing].currentTime)/(discs[playing].duration))*100}%`
-  
+    bar.style.width = `${((discs[playing].currentTime) / (discs[playing].duration)) * 100}%`
+
 
     window.requestAnimationFrame(barLenght);
-  };
+};
 
-function play(song){
+function play(song) {
 
     discs[playing].pause()
     discs[playing].currentTime = 0
+
+    playTextIsPlaying.innerHTML = "Play"
+    playTextIsPlaying.classList.remove("playing")
+
+    playTextIsPlaying = document.getElementById(song + "Play")
+    document.getElementById(song + "Play").innerHTML = "Playing..."
+    playTextIsPlaying.classList.add("playing")
+
+
     bar.style.width = "0%"
     discs[song].play()
     nowPlayingDiscName.innerText = song
@@ -70,19 +84,34 @@ function play(song){
     playing = song
     isPlaying = true
 
-   window.requestAnimationFrame(barLenght);
+    window.requestAnimationFrame(barLenght);
 
 }
 
-
-function playBar(){
+function playBar() {
     document.getElementById("play").classList.toggle("d-none")
     document.getElementById("pause").classList.toggle("d-none")
     discs[playing].play()
 }
 
-function pauseBar(){
+function pauseBar() {
     document.getElementById("play").classList.toggle("d-none")
     document.getElementById("pause").classList.toggle("d-none")
     discs[playing].pause()
+}
+
+function nextTrackBar() {
+    toPlay = autoPlayOrder[(autoPlayOrder.indexOf(playing)) + 1]
+    if ((autoPlayOrder.indexOf(playing)) == 18) {
+        toPlay = "Creator"
+    }
+    play(toPlay)
+}
+
+function previousTrackBar() {
+    toPlay = autoPlayOrder[(autoPlayOrder.indexOf(playing)) - 1]
+    if ((autoPlayOrder.indexOf(playing)) == 0) {
+        toPlay = "13"
+    }
+    play(toPlay)
 }
