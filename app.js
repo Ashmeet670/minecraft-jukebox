3a70f9dfeeed4fcf236ca03e565e395bb0f9c0bc
Ashmeet670 <73339721+Ashmeet670@users.noreply.github.com>
2024-09-15 16:15:41 +0530

    loading bar and more music cards

--- app.js
+++ app.js
@@ -1,6 +1,51 @@
 bar = document.getElementById("progress-bar")
 bar.style.width = "95%";
 
+discs = {
+    "5": document.getElementById("5"),
+    "11": document.getElementById("11"),
+    "13": document.getElementById("13"),
+    "blocks": document.getElementById("blocks"),
+    "cat": document.getElementById("cat"),
+    "chirp": document.getElementById("chirp"),
+    "creator_music_box": document.getElementById("creator_music_box"),
+    "creator": document.getElementById("creator"),
+    "mall": document.getElementById("mall"),
+    "mellohi": document.getElementById("mellohi"),
+    "otherside": document.getElementById("otherside"),
+    "pigstep": document.getElementById("pigstep"),
+    "precipice": document.getElementById("precipice"),
+    "record": document.getElementById("record"),
+    "relic": document.getElementById("relic"),
+    "stall": document.getElementById("stall"),
+    "strad": document.getElementById("strad"),
+    "wait": document.getElementById("wait"),
+    "ward": document.getElementById("ward"),
+}
+
+playing = "5"
+isPlaying = false
+barInterval = ""
+
+function barLenght() {
+    bar.style.width = `${((discs[playing].currentTime)/(discs[playing].duration))*100}%`
+  
+
+    window.requestAnimationFrame(barLenght);
+  };
+
+function play(song){
+
+    discs[playing].pause()
+    discs[playing].currentTime = 0
+    discs[song].play()
+    playing = song
+    isPlaying = true
+
+    window.requestAnimationFrame(progress_animation);
+
+}
+
 
 
 function togglePlayPause() {
@@ -8,45 +53,3 @@ function togglePlayPause() {
     document.getElementById("pause").classList.toggle("d-none")
     
 }
-
-playing = false
-nowPlaying = ''
-toPlay = ''
-var audio
-
-
-// function playSong(url){
-//     toPlay = url
-//     if(playing){
-//         audio.pause()
-//         console.log("paused")
-//     }
-//     else{
-//         console.log("playing")
-//         audio = new Audio(url).play()
-//         playing = true
-//     }
-// }
-
-// function playNow(){
-//     if(playing){
-//         console.log("playingNew")
-//         if(nowPlaying === toPlay){
-//             //do nothing
-//         }
-//         else if(nowPlaying != toPlay){
-
-//             console.log("pauseingn playing")
-
-//             audio.pause()
-//             audio.currentTime = 0
-//             playing = true
-//         }
-//     }
-//     else{
-//         console.log("playingNew1")
-//         audio = new Audio(toPlay).play()
-//         playing = true
-
-//     }
-// }
